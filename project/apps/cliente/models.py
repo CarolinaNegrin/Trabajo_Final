484c5b84737c9e79b9f324e186c81b8ac6344f78
Carolina Negrin <caro_negrin@hotmail.com>
2023-07-30 18:23:08 -0300

    modelo Cliente creado

--- project/apps/cliente/models.py
+++ project/apps/cliente/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length=20)
+    def __str__(self):
+        return self.nombre
+
+class Cliente (models.Model):
+    nombre = models.CharField(max_length=20, blank=False, null=False)
+    apellido = models.CharField(max_length=20, blank=False, null=False)
+    documento = models.IntegerField(max_length=8, blank=True, null=True)
+    contacto = models.IntegerField(max_length=15, blank=True, null=True)
+    domicilio = models.CharField(max_length=50, blank=True, null=True)
+    nacimiento = models.DateField(null=True)
+    ciudad = models.ForeignKey(Ciudad, blank=True, null=True)
+    def __str__(self):
+        return f"{self.apellido}, {self.nombre}"
