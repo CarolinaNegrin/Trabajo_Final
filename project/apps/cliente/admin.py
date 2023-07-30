28a4887e081119b45e69e7aae47b4bf4138d6b70
Carolina Negrin <caro_negrin@hotmail.com>
2023-07-30 19:51:12 -0300

    trabajando la interfaz

--- project/apps/cliente/admin.py
+++ project/apps/cliente/admin.py
@@ -3,5 +3,5 @@ from django.contrib import admin
 # Register your models here.
 from .models import Cliente, Ciudad
 
-admin.register(Cliente)
-admin.register(Ciudad)
+admin.site.register(Cliente)
+admin.site.register(Ciudad)
--- project/apps/home/admin.py
+++ project/apps/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.site_header = "Proyecto Final"
+admin.site.site_title = "Home"
