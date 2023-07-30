45611edd4db86738a6bc97686c4a3a4cfd08c4e6
Carolina Negrin <caro_negrin@hotmail.com>
2023-07-30 18:10:13 -0300

    aplicaciones y urls agregadas

--- project/config/settings.py
+++ project/config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'producto',
+    'venta',
+    'home',
+    'cliente',
 ]
 
 MIDDLEWARE = [
